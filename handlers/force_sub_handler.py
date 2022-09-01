63d5cafa8a4cb527f53905e44fe06e1bfd71f823
Trentyn <63451568+Trentyn@users.noreply.github.com>
2022-09-01 20:22:21 +0100

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -1,5 +1,3 @@
-# (c) @AbirHasan2005
-
 import asyncio
 from typing import (
     Union
@@ -32,7 +30,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/JoinOT).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Developer](https://t.me/Trentyn).",
                 disable_web_page_preview=True
             )
             return 400
@@ -61,7 +59,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+            text="Something went Wrong. Contact my [Developer](https://t.me/Trentyn).",
             disable_web_page_preview=True
         )
         return 200
