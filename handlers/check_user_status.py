65d4a9d6e0d5dbb784c34f95a00fa6d6979e308b
Trentyn <63451568+Trentyn@users.noreply.github.com>
2022-09-01 20:23:25 +0100

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -1,5 +1,3 @@
-# (c) Mr. Vishal & @AbirHasan2005
-
 import datetime
 from configs import Config
 from handlers.database import Database
@@ -23,6 +21,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
+            await cmd.reply_text("You are Banned to Use This Bot", quote=True)
             return
     await cmd.continue_propagation()
